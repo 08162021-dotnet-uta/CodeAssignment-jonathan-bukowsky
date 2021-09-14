bf608e1fcf66a65ee980e98c8b3a13a309db8530
Jonathan Bukowsky <jonathan.bukowsky@revature.net>
2021-09-14 10:15:23 -0400

    Create SweetNSalty project

---
+++ SweetNSalty/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SweetNSalty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 100; i += 10)
+            {
+
+            }
+        }
+    }
+}
